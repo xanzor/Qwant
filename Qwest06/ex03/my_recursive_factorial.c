1f49cda4b4f64cf54080bcc5644feee3c7f306ee
Anzor <69747272+xanzor@users.noreply.github.com>
2020-10-06 21:35:29 +0300

    //

---
+++ Qwest06/ex03/my_recursive_factorial.c
@@ -0,0 +1,9 @@
+int my_recursive_factorial(int param_1)
+{
+    if (param_1 < 0)
+        return 0;
+
+    if (param_1 > 0)
+        if (param_1 == 0 || param_1 == 1) return 1;
+            return param_1 * my_recursive_factorial(param_1 - 1);
+}
