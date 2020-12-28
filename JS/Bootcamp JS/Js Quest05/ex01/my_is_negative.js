124ff63ea470e51ca310772258faf7fb00f25567
Anzor <69747272+xanzor@users.noreply.github.com>
2020-12-28 13:28:13 +0300

    js

---
+++ JS/Bootcamp JS/Js Quest05/ex01/my_is_negative.js
@@ -0,0 +1,14 @@
+function my_is_negative(n) {
+  if (n >= 0) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+};
+
+// console.log(my_is_negative(-1));
+// console.log(my_is_negative(1));
+// console.log(my_is_negative(0));
+
+// console.log(my_is_negative(1337));
